0921559a336fed068ec8c92ff1b105889cadff63
Nirosh Madushan <127856472+niroshmadushan@users.noreply.github.com>
2024-09-27 16:07:43 +0530

    ssss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// src/App.js
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme/theme';
@@ -12,80 +11,33 @@ import AddMeetingInternal from './pages/AddMeetingInternal';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
 import Register from './pages/RegistrationPage';
-import Layout from './pages/Layout'; // Import Layout
+import Layout from './pages/Layout';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { AuthProvider } from './pages/AuthContext'; // Import AuthContext
-import PrivateRoute from './components/PrivateRoute'; // Import PrivateRoute from your separate file
+import { AuthProvider, AuthContext } from './pages/AuthContext'; // Make sure this import is correct
+import PrivateRoute from './components/PrivateRoute';
 
 const AppContent = () => {
+  const { isAuthenticated } = useContext(AuthContext); // Use the context
+
   return (
     <Routes>
+      {/* Redirect if authenticated */}
+      {isAuthenticated && (
+        <Route path="/" element={<Navigate replace to="/connex_meet_emp/dash" />} />
+      )}
+
       {/* Public routes */}
       <Route path="/connex_meet_emp/" element={<Login />} />
       <Route path="/connex_meet_emp/reg" element={<Register />} />
 
       {/* Protected routes */}
-      <Route
-        path="/connex_meet_emp/dash"
-        element={
-          <PrivateRoute>
-            <Layout>
-              <HomeDashboard />
-            </Layout>
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/connex_meet_emp/eventspage"
-        element={
-          <PrivateRoute>
-            <Layout>
-              <Home />
-            </Layout>
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/connex_meet_emp/meeting-rooms"
-        element={
-          <PrivateRoute>
-            <Layout>
-              <MeetingRooms />
-            </Layout>
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/connex_meet_emp/addmeeting"
-        element={
-          <PrivateRoute>
-            <Layout>
-              <AddMeetingSession />
-            </Layout>
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/connex_meet_emp/addInternalMeeting"
-        element={
-          <PrivateRoute>
-            <Layout>
-              <AddMeetingInternal />
-            </Layout>
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/connex_meet_emp/profile"
-        element={
-          <PrivateRoute>
-            <Layout>
-              <Profile />
-            </Layout>
-          </PrivateRoute>
-        }
-      />
+      <Route path="/connex_meet_emp/dash" element={<PrivateRoute><Layout><HomeDashboard /></Layout></PrivateRoute>} />
+      <Route path="/connex_meet_emp/eventspage" element={<PrivateRoute><Layout><Home /></Layout></PrivateRoute>} />
+      <Route path="/connex_meet_emp/meeting-rooms" element={<PrivateRoute><Layout><MeetingRooms /></Layout></PrivateRoute>} />
+      <Route path="/connex_meet_emp/addmeeting" element={<PrivateRoute><Layout><AddMeetingSession /></Layout></PrivateRoute>} />
+      <Route path="/connex_meet_emp/addInternalMeeting" element={<PrivateRoute><Layout><AddMeetingInternal /></Layout></PrivateRoute>} />
+      <Route path="/connex_meet_emp/profile" element={<PrivateRoute><Layout><Profile /></Layout></PrivateRoute>} />
     </Routes>
   );
 };
