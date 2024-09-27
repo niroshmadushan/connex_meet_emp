719e52ab79dd1884ab7cd2629b265c88e07c5456
Nirosh Madushan <127856472+niroshmadushan@users.noreply.github.com>
2024-09-27 16:16:39 +0530

    ssss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -14,51 +14,30 @@ import Register from './pages/RegistrationPage';
 import Layout from './pages/Layout';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { AuthProvider, AuthContext } from './pages/AuthContext'; // Make sure the path is correct
-import PrivateRoute from './components/PrivateRoute'; // Make sure the path is correct
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import { AuthProvider, AuthContext } from './pages/AuthContext'; // Make sure this import is correct
+import PrivateRoute from './components/PrivateRoute';
 
 const AppContent = () => {
-  const { isAuthenticated } = useContext(AuthContext);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Only show the loading screen if authenticated, for a delay before redirection
-    if (isAuthenticated) {
-      setTimeout(() => {
-        setIsLoading(false);
-      }, 5000); // 5 seconds loading screen
-    } else {
-      setIsLoading(false);
-    }
-  }, [isAuthenticated]);
-
-  if (isLoading) {
-    return (
-      <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-        <CircularProgress />
-      </Box>
-    );
-  }
+  const { isAuthenticated } = useContext(AuthContext); // Use the context
 
   return (
     <Routes>
-      {/* Redirect if authenticated and loading is done */}
-      {isAuthenticated ? (
-        <Route path="*" element={<Navigate replace to="/connex_meet_emp/dash" />} />
-      ) : (
-        <>
-          <Route path="/connex_meet_emp/" element={<Login />} />
-          <Route path="/connex_meet_emp/reg" element={<Register />} />
-          <Route path="/connex_meet_emp/dash" element={<PrivateRoute><Layout><HomeDashboard /></Layout></PrivateRoute>} />
-          <Route path="/connex_meet_emp/eventspage" element={<PrivateRoute><Layout><Home /></Layout></PrivateRoute>} />
-          <Route path="/connex_meet_emp/meeting-rooms" element={<PrivateRoute><Layout><MeetingRooms /></Layout></PrivateRoute>} />
-          <Route path="/connex_meet_emp/addmeeting" element={<PrivateRoute><Layout><AddMeetingSession /></Layout></PrivateRoute>} />
-          <Route path="/connex_meet_emp/addInternalMeeting" element={<PrivateRoute><Layout><AddMeetingInternal /></Layout></PrivateRoute>} />
-          <Route path="/connex_meet_emp/profile" element={<PrivateRoute><Layout><Profile /></Layout></PrivateRoute>} />
-        </>
+      {/* Redirect if authenticated */}
+      {isAuthenticated && (
+        <Route path="connex_meet_emp/" element={<Navigate replace to="/connex_meet_emp/dash" />} />
       )}
+
+      {/* Public routes */}
+      <Route path="/connex_meet_emp/" element={<Login />} />
+      <Route path="/connex_meet_emp/reg" element={<Register />} />
+
+      {/* Protected routes */}
+      <Route path="/connex_meet_emp/dash" element={<PrivateRoute><Layout><HomeDashboard /></Layout></PrivateRoute>} />
+      <Route path="/connex_meet_emp/eventspage" element={<PrivateRoute><Layout><Home /></Layout></PrivateRoute>} />
+      <Route path="/connex_meet_emp/meeting-rooms" element={<PrivateRoute><Layout><MeetingRooms /></Layout></PrivateRoute>} />
+      <Route path="/connex_meet_emp/addmeeting" element={<PrivateRoute><Layout><AddMeetingSession /></Layout></PrivateRoute>} />
+      <Route path="/connex_meet_emp/addInternalMeeting" element={<PrivateRoute><Layout><AddMeetingInternal /></Layout></PrivateRoute>} />
+      <Route path="/connex_meet_emp/profile" element={<PrivateRoute><Layout><Profile /></Layout></PrivateRoute>} />
     </Routes>
   );
 };
